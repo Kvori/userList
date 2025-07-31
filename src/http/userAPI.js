8c04a8cae6f60755f311db5363b14c332c0d020c
Vadim <kvori777@mail.ru>
2025-07-31 17:40:56 +0300

    midified URL

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,21 +1,21 @@
 import { $authHost, $host } from "./index.js"
 
 export const registration = async (email, name, password) => {
-    const {data} = await $host.post('/users/registration', {email, name, password})
+    const {data} = await $host.post('api/users/registration', {email, name, password})
     localStorage.setItem('token', data.token)
     const user = data.user
     return user
 }
 
 export const login = async (email, password) => {
-    const {data} = await $host.post('/users/login', {email, password})
+    const {data} = await $host.post('api/users/login', {email, password})
     localStorage.setItem('token', data.token)
     const user = data.user
     return user
 }
 
 export const check = async () => {
-    const {data} = await $authHost.get('/users/auth')
+    const {data} = await $authHost.get('api/users/auth')
     localStorage.setItem('token', data.token)
     const user = data.user
     return user
--- src/http/userListAPI.js
+++ src/http/userListAPI.js
@@ -1,21 +1,21 @@
 import { $authHost } from "./index.js";
 
 export const getUserList = async () => {
-    const {data} = await $authHost.get('/users/data')
+    const {data} = await $authHost.get('api/users/data')
     return data
 }
 
 export const deleteUser = async (id) => {
-    const response = await $authHost.delete(`/users/delete?id=${id}`)
+    const response = await $authHost.delete(`api/users/delete?id=${id}`)
     return response
 }
 
 export const blockUser = async (id) => {
-    const response = await $authHost.get(`/users/block?id=${id}`)
+    const response = await $authHost.get(`api/users/block?id=${id}`)
     return response
 }
 
 export const unblockUser = async (id) => {
-    const response = await $authHost.get(`/users/unblock?id=${id}`)
+    const response = await $authHost.get(`api/users/unblock?id=${id}`)
     return response
 }
